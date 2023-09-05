fbe9fa2d573e89332c7a29a03ffad9bfac54879d
leuhan-c <84480064+leuhan-c@users.noreply.github.com>
2023-09-05 17:49:48 -0300

    DOM

---
+++ desafioDOM/script.js
@@ -0,0 +1,64 @@
+/*-->Variable miAuto
+Creá una Variable llamada miAuto y asignale un Objeto vacío.
+Asignale a miAuto una Propiedad marca, que a su vez contenga un String.
+Agregá una Propiedad año que contenga un número entero.
+Agregá una Propiedad nuevo que contenga un booleano.
+Creá una Variable llamada propertyKey y asignale el String "modelo"
+Agregá la Propiedad modelo al Objeto miAuto
+Creá una Variable llamada anotherPropertyKey y dale el Valor "precio".
+Cuantificá el Valor de tu auto.
+Asigná la String "color" a una Variable nueva llamada nextProperty.
+Usá la variable nextProperty para añadir el color a tu auto.
+Usá el loop for...in para iterar sobre el Objeto miAuto y que muestre en la consola el key y el value de cada Propiedad.
+
+let miAuto = {}
+miAuto["marca"] = "Fiat"
+miAuto["año"] = 2023
+miAuto["nuevo"] = true
+let propertyKey = "modelo"
+miAuto[propertyKey]="Argo"
+let anotherPropertyKey = "precio"
+miAuto[propertyKey]=3000000000
+let nextProperty = "color"
+miAuto[nextProperty]="negro"
+
+for (let propiedad in miAuto) {
+    console.log(propiedad + ": " + miAuto[propiedad])
+}
+-------------------
+
+*/
+
+let miAuto = {}
+miAuto["marca"] = "Fiat"
+miAuto["año"] = 2023
+miAuto["nuevo"] = true
+let propertyKey = "modelo"
+miAuto[propertyKey]="Argo"
+let anotherPropertyKey = "precio"
+miAuto[propertyKey]=3000000000
+let nextProperty = "color"
+miAuto[nextProperty]="negro"
+
+for (let propiedad in miAuto) {
+    console.log(propiedad)
+}
+
+
+/*
+var miAuto = {
+
+}
+
+miAuto.marca="Volkswagen"
+miAuto.año=2018
+miAuto["nuevo"]=true
+var propertyKey = "modelo"
+miAuto[propertyKey]="Sharan"
+var anotherPropertyKey="precio"
+miAuto[anotherPropertyKey]="USD 280.500.000"
+let nextProperty="color"
+miAuto[nextProperty]="Amarillo"
+
+
+*/
