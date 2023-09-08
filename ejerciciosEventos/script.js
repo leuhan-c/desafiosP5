08661d73826a312f72462dfea74aaeb275a65254
leuhan-c <84480064+leuhan-c@users.noreply.github.com>
2023-09-08 16:35:54 -0300

    .

---
+++ ejerciciosEventos/script.js
@@ -0,0 +1,6 @@
+/*
+Poné una imagen en tu página que desaparezca cuando pases el mouse por arriba de un <div> secreto llamado #magia.
+*/
+let img = document.querySelector("img")
+let div = document.querySelector("div")
+
